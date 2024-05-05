d90a24990988a8141dabc8fe984d1080d384dbfa
Piersatile <ewanpierre@gmail.com>
2024-05-05 14:54:29 +0200

    revérification et finalisation du code

--- 02_PROTEGO_PCS_SEXE.R
+++ 02_PROTEGO_PCS_SEXE.R
@@ -1,7 +1,4 @@
 #tri croisés de PROTEGO avec variables d'état (PCS et sexe)
-library(questionr)
-library(foreign)
-library(ggplot2)
 
 table (bnum22$PROTEGO, useNA="ifany")
 
@@ -40,7 +37,7 @@ chisq.test(table(bnum22$PROTEGO, bnum22$PCSc, useNA="ifany")) #on peut
 table(bnum22$SEXE, useNA="ifany")
 table (bnum22$PROTEGO, bnum22$SEXE, useNA="ifany")
 lprop(table(bnum22$PROTEGO, bnum22$SEXE, useNA="ifany"))
-chisq.test(table (bnum22$PROTEGO, bnum22$SEXE, useNA="ifany"))
+chisq.test(table (bnum22$PROTEGO, bnum22$SEXE, useNA="ifany")) #c'est juste
 
 #tableaux PROTEGO et PCSc
 write.csv2(table(bnum22$PCSc, bnum22$PROTEGO, useNA="ifany"), 
@@ -48,13 +45,14 @@ write.csv2(table(bnum22$PCSc, bnum22$PROTEGO, useNA="ifany"),
 write.csv2(lprop(table(bnum22$PCSc, bnum22$PROTEGO, useNA="ifany")), 
            file="Sorties/PROTE_PCS_lprop.csv", fileEncoding = "UTF-8")
 
-#diagrammes à barre PROTEGO et SEXE
+#diagrammes à barre PROTEGO et SEXE 
+#non utilisé car les données de la variable SEXE ne donne pas vraiment d'explication à la vigilance
 PROTSEXE <- ggplot(bnum22) + aes(x=PROTEGO, weight=POND, fill=SEXE) +
   geom_bar(position="fill") + 
   labs(title="Diagramme à barre de l'attention à la protection des données personnelles selon le sexe",
        x="Attention à la protection des données personnelles", y="Effectif (en %)",
        caption="Source : Baromètre du numérique 2022") + theme_bw()
-ggsave("Sorties/yipee.png", plot=PROTSEXE, width=9, height=4)
+ggsave("Sorties/PROTSEXE.png", plot=PROTSEXE, width=9, height=4)
 
 #diagramme à barres PCS PROTEGO
 
@@ -64,30 +62,5 @@ graph_export <- ggplot(bnum22, aes(x = PCSc, fill = PROTEGO)) +
   ggtitle("Vigilance envers les données par groupe socioprofessionnel") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-#bon en fait il sert pas à grand chose
 
 ggsave("sorties/Graphique_PCS.png", plot = graph_export, width = 6, height = 3, bg = "white")
-
-#Cookies ?
-table(bnum22$COOKIE, useNA="ifany")
-## Recodage de bnum22$COOKIE
-bnum22$COOKIE_rec <- bnum22$COOKIE
-bnum22$COOKIE_rec[bnum22$COOKIE == "Le moins souvent possible"] <- "Peu"
-bnum22$COOKIE_rec[bnum22$COOKIE == "N'a pas internet fixe à domicile"] <- "N'a pas internet"
-bnum22$COOKIE_rec[bnum22$COOKIE == "Régulièrement"] <- "Souvent"
-bnum22$COOKIE_rec[bnum22$COOKIE == "Systématiquement"] <- "Toujours"
-
-## Réordonnancement de bnum22$COOKIE_rec
-bnum22$COOKIE_rec <- factor(bnum22$COOKIE_rec,
-  levels = c("N'a pas internet", "Jamais", "Peu", "Souvent", "Toujours")
-)
-
-#tri croisé
-table(bnum22$PROTEGO, bnum22$COOKIE_rec, useNA="ifany")
-lprop(table(bnum22$PROTEGO, bnum22$COOKIE_rec, useNA="ifany"))
-chisq.test(table(bnum22$PROTEGO, bnum22$COOKIE_rec, useNA="ifany"))
-cookieprot <- table(bnum22$PROTEGO, bnum22$COOKIE_rec, useNA="ifany")
-cramer.v(cookieprot)
-ggplot(bnum22) + aes(x=COOKIE_rec, weights=POND, fill=PROTEGO) +
-  geom_bar(position="dodge") + labs(x="Acceptation des cookies", y="Effectifs",
-                                    fill="Attention à la protection des données")
--- 03_PROTEGO_WEBATTAQ.R
+++ 03_PROTEGO_WEBATTAQ.R
@@ -1,21 +1,14 @@
-library("questionr")
-library("foreign")
-
-bnum <- barometre_du_numerique_2007_2022
-rm(barometre_du_numerique_2007_2022)
+#Tri croisé PROTEGO et WEBATTAQ
 
 table(bnum$WEBATTAQ)
 warnings(bnum$WEBATTAQ)
 table(bnum$WEBATTAQ,bnum$ANNEE)
 
-bnum22 <- barometre_du_numerique_2022
-rm(barometre_du_numerique_2022)
-
 table(bnum22$WEBATTAQ)
 
 table(bnum22$WEBATTAQ, bnum22$PROTEGO, useNA = "ifany")
 
-bnum22$ATTAQ <- NA
+bnum22$ATTAQ <- bnum22$WEBATTAQ
 bnum22$ATTAQ[bnum22$WEBATTAQ=="Non, ce n\x92est pas possible"] <- "N'a jamais subi d'attaque sur ses données"
 bnum22$ATTAQ[bnum22$WEBATTAQ=="Non, c\x92est peu probable"]<- "Ne pense pas avoir subi d'attaque sur ses données"
 bnum22$ATTAQ[bnum22$WEBATTAQ=="Oui, c\x92est probable"]<- "Pense avoir subi une attaque sur ses données"
@@ -23,7 +16,7 @@ bnum22$ATTAQ[bnum22$WEBATTAQ=="Oui, c\x92est s\xfbr"]<- "A déjà subi une attaq
 
 table(bnum22$ATTAQ)
 
-bnum22$VIGI <- NA
+bnum22$VIGI <- bnum22$PROTEGO
 bnum22$VIGI[bnum22$PROTEGO=="N'a pas internet fixe \xe0 domicile"] <- "N'a pas internet"
 bnum22$VIGI[bnum22$PROTEGO=="Non"]<- "Pas vigilant"
 bnum22$VIGI[bnum22$PROTEGO=="Oui, et je suis tr\xe8s vigilant"]<- "Très vigilant"
@@ -37,11 +30,5 @@ lprop(table(bnum22$ATTAQ, bnum22$VIGI, useNA = "ifany"))
 
 chisq.test(table(bnum22$ATTAQ, bnum22$VIGI, useNA = "ifany"))
 
-write.csv2(lprop(table(bnum22$ATTAQ, bnum22$VIGI, useNA = "ifany"))
-)
-
 write.csv2(lprop(table(bnum22$ATTAQ, bnum22$VIGI, useNA = "ifany")), 
-           file = "ATTAQxVIGI.csv")
-
-
-
+           file = "Sorties/ATTAQxVIGI.csv")
